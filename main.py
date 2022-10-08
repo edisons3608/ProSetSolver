7f6b105bea073ab0818b26637d9a46e1a9a36aeb
edisons3608 <57467707+edisons3608@users.noreply.github.com>
2022-10-08 12:33:53 -0500

    sfd

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from itertools import chain, combinations
+from re import I
+
+from sympy import true
 
 def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
@@ -6,7 +9,7 @@ def powerset(iterable):
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 
-possible = list(powerset([1,2,3,4,5,6]))
+possible = list(powerset([1,2,3,4,5,6,7]))
 
 modified = [];
 for x in range(len(possible)):
@@ -20,13 +23,40 @@ def xorList(list1,list2):
         returnlist.append(list1[x] ^ list2[x])
 
     return returnlist
-p1 = [0,1,0,1,0,1]
-p2 = [1,1,0,1,1,0]
-p3 = [1,1,0,0,0,1]
-p4 = [1,0,1,1,0,0]
-p5 = [1,0,0,1,0,0]
-p6 = [1,1,0,1,0,1]
-p7 = [0,1,1,1,1,0]
+p1 = [0,0,0,0,0,0]
+p2 = [0,0,0,0,0,0]
+p3 = [0,0,0,0,0,0]
+p4 = [0,0,0,0,0,0]
+p5 = [0,0,0,0,0,0]
+p6 = [0,0,0,0,0,0]
+p7 = [0,0,0,0,0,0]
+
+input1 = ""
+flag = True
+counter = 1
+while counter < 8:
+    input1 = input("p"+str(counter)+": ")
+    
+    if input1 != "end":
+        #print(int(input1))
+        if counter == 1:
+            p1[int(input1)-1] = 1
+            
+        elif counter == 2:
+            p2[int(input1)-1] = 1
+        elif counter == 3:
+            p3[int(input1)-1] = 1
+        elif counter == 4:
+            p4[int(input1)-1] = 1
+        elif counter == 5:
+            p5[int(input1)-1] = 1
+        elif counter == 6:
+            p6[int(input1)-1] = 1
+        elif counter == 7:
+            p7[int(input1)-1] = 1
+    else:
+        counter += 1
+        #print(p1)
 picDict = {1:p1, 2:p2, 3:p3, 4:p4, 5:p5, 6:p6, 7:p7}
 for x in range(len(modified)):
     current = modified[x];
